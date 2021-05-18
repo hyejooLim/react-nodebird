a7089c7f81c8ed4faa4288624c14a6d557987b0c
hyejooLim <lhyejoo@naver.com>
2021-05-18 18:23:53 +0900

    chore: 프로젝트 초기 설정

---
+++ front/pages/profile.js
@@ -0,0 +1,11 @@
+import AppLayout from '../components/AppLayout';
+
+const Profile = () => {
+  return (
+    <AppLayout>
+      <div>my profile</div>
+    </AppLayout>
+  );
+};
+
+export default Profile;
---
+++ front/pages/signup.js
@@ -0,0 +1,11 @@
+import AppLayout from '../components/AppLayout';
+
+const Signup = () => {
+  return (
+    <AppLayout>
+      <div>signup page</div>
+    </AppLayout>
+  );
+};
+
+export default Signup;
