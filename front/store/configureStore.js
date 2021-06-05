6b9cbf73064d36a2786ab7800b530a42ff39f32f
hyejooLim <lhyejoo@naver.com>
2021-06-05 18:45:46 +0900

    Add comment

--- front/store/configureStore.js
+++ front/store/configureStore.js
@@ -6,10 +6,9 @@ import createSagaMiddleware from 'redux-saga';
 import reducer from '../reducers';
 import rootSaga from '../sagas';
 
-// redux thunk (액션을 함수로 둘 수 있음)
 // 액션이 디스패치되는 것을 로깅하는 미들웨어
 const loggerMiddleware = ({ dispatch, getState }) => (next) => (action) => {
-  console.log(action); // 액션을 실행하기 전에 콘솔 찍어주는 미들웨어
+  console.log(action); // 액션을 실행하기 전에 콘솔 창에 액션을 찍어줌
   return next(action);
 }
 
@@ -21,11 +20,11 @@ const configureStore = () => {
   : composeWithDevTools(applyMiddleware(...middlewares));
 
   const store = createStore(reducer, enhancer);
-  store.sagaTask = sagaMiddleware.run(rootSaga); // Run rootSaga after create store
+  store.sagaTask = sagaMiddleware.run(rootSaga); // store를 생성한 이후에 rootSaga 실행
   return store;
 }
 
-// when you develop, set debug true
+// 개발 모드일 경우 debug를 true로 설정
 const wrapper = createWrapper(configureStore, {
   debug: process.env.NODE_ENV === 'development',
 });
