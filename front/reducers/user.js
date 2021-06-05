a18f985a2bcf54495965c1388e7ef13ac276540f
hyejooLim <lhyejoo@naver.com>
2021-06-05 18:33:32 +0900

    Add: login/logout actions

--- front/reducers/user.js
+++ front/reducers/user.js
@@ -1,36 +1,62 @@
 const initialState = {
+  isLoggingIn: false, // 로그인 시도 중
   isLoggedIn: false,
+  isLoggingOut: false, // 로그아웃 시도 중
   user: null,
   signUpData: {},
   loginData: {},
 };
 
-export const loginAction = (data) => {
+// Action creator
+export const loginRequestAction = (data) => {
   return {
-    type: 'LOG_IN',
+    type: 'LOG_IN_REQUEST',
     data,
   };
 };
 
-export const logoutAction = () => {
+export const logoutRequestAction = () => {
   return {
-    type: 'LOG_OUT',
+    type: 'LOG_OUT_REQUEST',
   };
 };
 
 const reducer = (state = initialState, action) => {
   switch (action.type) {
-    case 'LOG_IN':
+    case 'LOG_IN_REQUEST':
       return {
         ...state,
-        isLoggedIn: true,
-        user: action.data,
+        isLoggingIn: true,
       };
-    case 'LOG_OUT':
+    case 'LOG_IN_SUCCESS':
       return {
         ...state,
+        isLoggingIn: false,
+        isLoggedIn: true,
+        user: { ...action.data, nickname: 'joo' },
+      };
+    case 'LOG_IN_FAILURE':
+      return {
+        ...state,
+        isLoggingIn: false,
+        isLoggedIn: false,
+      };
+    case 'LOG_OUT_REQUEST':
+      return {
+        ...state,
+        isLoggingOut: true,
+      };
+    case 'LOG_OUT_SUCCESS':
+      return {
+        ...state,
+        isLogginOut: false,
         isLoggedIn: false,
         user: null,
+      };
+    case 'LOG_OUT_FAILURE':
+      return {
+        ...state,
+        isLoggingOut: false,
       };
     default:
       return state;
