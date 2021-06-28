f5130f8c7ad64395d75b012e089d72f0e0b483b7
hyejooLim <lhyejoo@naver.com>
2021-06-28 18:03:44 +0900

    Feat: search hashtag

--- front/components/AppLayout.js
+++ front/components/AppLayout.js
@@ -1,19 +1,26 @@
-import React from 'react';
+import React, { useCallback } from 'react';
 import Link from 'next/link';
 import { Menu, Input, Row, Col } from 'antd';
 import styled from 'styled-components';
 import { useSelector } from 'react-redux';
+import Router from 'next/router';
 
 import LoginForm from '../components/LoginForm';
 import UserProfile from '../components/UserProfile';
+import useInput from '../hooks/useInput';
 
 const SearchInput = styled(Input.Search)`
   vertical-align: middle;
 `;
 
 const AppLayout = ({ children }) => {
+  const [searchInput, onChangeSearchInput] = useInput('');
   const { user } = useSelector((state) => state.user);
   
+  const onSearch = useCallback(() => {
+    Router.push(`/hashtag/${searchInput}`);
+  }, [searchInput]);
+
   return (
     <div>
       <Menu mode='horizontal'>
@@ -24,7 +31,12 @@ const AppLayout = ({ children }) => {
           <Link href='/profile'><a>프로필</a></Link>
         </Menu.Item>
         <Menu.Item>
-          <SearchInput enterButton />
+          <SearchInput 
+            enterButton
+            value={searchInput}
+            onChange={onChangeSearchInput}
+            onSearch={onSearch} 
+            />
         </Menu.Item>
         <Menu.Item>
           <Link href='/signup'><a>회원가입</a></Link>
